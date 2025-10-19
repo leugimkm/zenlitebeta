c12184fc414cec34ca5b6283d12d11a41d212ebb
leugimkm <leugimkm@systrien.com>
2025-10-18 22:32:26 -0500

    u: changing fix

--- lua/zenlite/init.lua
+++ lua/zenlite/init.lua
@@ -1,85 +1,105 @@
 local config = require("zenlite.config")
-local util = require("zenlite.util")
+local utils = require("zenlite.util")
 local plugins = require("zenlite.plugins")
 
 local M = { _active = false, _saved = {}, opts = {} }
 
 local function enter()
-	for name in pairs(M.opts.options) do
-		util.save_option(M._saved, name)
-		util.apply_option(M.opts.options, name, false)
-	end
+  for name in pairs(M.opts.options) do
+    util.save_option(M._saved, name)
+    util.apply_option(M.opts.options, name, false)
+  end
 
-	plugins.toggle_diagnostics(false)
-	if M.opts.twilight.enable then
-		plugins.toggle_twilight(true)
-	end
+  if M.opts.diagnostics.enable then
+    plugins.toggle_diagnostics(false)
+  end
 
-	if type(M.opts.custom_on) == "function" then
-		M.opts.custom_on()
-	end
+  if M.opts.twilight.enable then
+    plugins.toggle_twilight(true)
+  end
+
+  if type(M.opts.custom_on) == "function" then
+    M.opts.custom_on()
+  end
 end
 
 local function exit()
-	for name in pairs(M.opts.options) do
-		util.restore_option(M._saved, name)
-	end
+  for name in pairs(M.opts.options) do
+    util.restore_option(M._saved, name)
+  end
 
-	plugins.toggle_diagnostics(true)
-	if M.opts.twilight.enable then
-		plugins.toggle_twilight(false)
-	end
+  if M.opts.diagnostics.enable then
+    plugins.toggle_diagnostics(true)
+  end
 
-	if type(M.opts.custom_off) == "function" then
-		M.opts.custom_off()
-	end
+  if M.opts.twilight.enable then
+    plugins.toggle_twilight(false)
+  end
+
+  if type(M.opts.custom_off) == "function" then
+    M.opts.custom_off()
+  end
 end
 
 function M.toggle()
-	M._active = not M._active
-	if M._active then
-		enter()
-	else
-		exit()
-	end
+  M._active = not M._active
+  if M._active then
+    enter()
+  else
+    exit()
+  end
 end
 
 function M.toggle_option(name)
-	if not M.opts.options[name] then
-		return
-	end
+  if not M.opts.options[name] then
+    return
+  end
 
-	local current = vim.opt[name]:get()
-	local is_on = current == M.opts.options[name].on
-	local new_state = not is_on
+  local current = vim.opt[name]:get()
+  local is_on = current == M.opts.options[name].on
+  local new_state = not is_on
 
-	util.apply_option(M.opts.options, name, new_state)
+  util.apply_option(M.opts.options, name, new_state)
 
-	if name == "number" then
-		util.apply_option(M.opts.options, "relativenumber", new_state)
-		util.apply_option(M.opts.options, "colorcolumn", new_state)
-		local sc = M.opts.options.signcolumn
-		if sc then
-			vim.opt.signcolumn = new_state and sc.on or sc.off
-		end
-	end
+  if name == "number" then
+    util.apply_option(M.opts.options, "relativenumber", new_state)
+    util.apply_option(M.opts.options, "colorcolumn", new_state)
+    util.apply_option(M.opts.options, "signcolumn", new_state)
+  end
 end
 
 function M.toggle_all()
-	local status_active = vim.opt.laststatus:get() == M.opts.options.laststatus.on
-	local linenr_active = vim.opt.number:get()
-	local diagnostics_active = vim.diagnostic.is_enabled()
+  local status_on = vim.opt.laststatus:get() == M.opts.options.laststatus.on
+  local new_state = not status_on
 
-	local new_state = not (status_active and linenr_active and diagnostics_active)
+  util.apply_option(M.opts.options, "laststatus", new_state)
+  M.toggle_option("number")
 
-	util.apply_option(M.opts.options, "laststatus", new_state)
-	M.toggle_option("number")
-	plugins.toggle_diagnostics(new_state)
+  local diag_state = vim.diagnostic.is_enabled()
+  vim.diagnostic.enable(not diag_state)
 end
 
 function M.setup(user_opts)
-	M.opts = config.load(user_opts)
-	plugins.register_keymaps(M.opts, M.toggle, M.toggle_option, M.toggle_all)
+  M.opts = config.load(user_opts)
+
+  local km = M.opts.keymaps
+  vim.keymap.set("n", km.zen, M.toggle, { desc = "Toggle zen mode" })
+  vim.keymap.set("n", km.all, M.toggle_all, { desc = "Toggle all (statusline, lineNr, diagnostics)" })
+  vim.keymap.set("n", km.statusline, function() M.toggle_option("laststatus") end, { desc = "Toggle statusline" })
+  vim.keymap.set("n", km.linenr, function() M.toggle_option("number") end, { desc = "Toggle line numbers" })
+  vim.keymap.set("n", km.diagnostics, function()
+    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+  end, { desc = "Toggle diagnostics" })
+  vim.keymap.set("n", km.virtuallines, function()
+    plugins.toggle_virtual("lines")
+  end, { desc = "Toggle diagnostic virtual_lines" })
+  vim.keymap.set("n", km.virtualtext, function()
+    plugins.toggle_virtual("text")
+  end, { desc = "Toggle diagnostic virtual_text" })
+
+  vim.api.nvim_create_user_command("ZenModeToggle", function()
+    M.toggle()
+  end, {})
 end
 
 return M
--- lua/zenlite/plugins.lua
+++ lua/zenlite/plugins.lua
@@ -1,38 +1,28 @@
 local M = {}
 
+function M.has_twilight()
+  local ok, _ = pcall(require, "twilight")
+  return ok
+end
+
+function M.toggle_twilight(state)
+  if not M.has_twilight() then
+    return
+  end
+  vim.cmd(state and "TwilightEnable" or "TwilightDisable")
+end
+
 function M.toggle_diagnostics(state)
   vim.diagnostic.enable(state)
 end
 
-function M.toggle_twilight(state)
-  local ok, _ = pcall(require, "twilight")
-  if not ok then
-    return
+function M.toggle_virtual(kind)
+  local cfg = vim.diagnostic.config()
+  if kind == "lines" then
+    vim.diagnostic.config({ virtual_lines = not cfg.virtual_lines })
+  elseif kind == "text" then
+    vim.diagnostic.config({ virtual_text = not cfg.virtual_text })
   end
-  if state then
-    vim.cmd("TwilightEnable")
-  else
-    vim.cmd("TwilightDisable")
-  end
-end
-
-function M.register_keymaps(opts, toggle_fn, toggle_option_fn, toggle_all_fn)
-  local km = opts.keymaps
-  vim.keymap.set("n", km.zen, toggle_fn, { desc = "Toggle zen mode" })
-  vim.keymap.set("n", km.all, M.toggle_all, { desc = "Toggle all (statusline, lineNr, diagnostics)" })
-  vim.keymap.set("n", km.statusline, function() toggle_option_fn("laststatus") end, { desc = "Toggle statusline" })
-  vim.keymap.set("n", km.linenr, function() toggle_option_fn("number") end, { desc = "Toggle line numbers" })
-  vim.keymap.set("n", km.diagnostics, function()
-    vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-  end, { desc = "Toggle diagnostics" })
-  vim.keymap.set("n", km.virtuallines, function()
-    local state = not vim.diagnostic.config().virtual_lines
-    vim.diagnostic.config({ virtual_lines = state })
-  end, { desc = "Toggle diagnostic virtual_lines" })
-  vim.keymap.set("n", km.virtualtext, function()
-    local state = not vim.diagnostic.config().virtual_text
-    vim.diagnostic.config({ virtual_text = state })
-  end, { desc = "Toggle diagnostic virtual_text" })
 end
 
 return M
